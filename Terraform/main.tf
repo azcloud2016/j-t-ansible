28710608eb2abbf3b0774961bfec0a61a4870a5e
azcloud2016 <122769000+azcloud2016@users.noreply.github.com>
2023-01-21 14:58:15 -0500

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -15,7 +15,7 @@ data "aws_ami" "amazon-linux" {
 resource "aws_instance" "dev_machine" {
   ami = data.aws_ami.amazon-linux.id
   instance_type = "t2.micro"
-  key_name = "aws-jenkins"
+  key_name = "aws-jenkins-2"
 
   tags = {
     Environment = "dev"
